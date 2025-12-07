ebcc28eee11599fcab551977771f080ffac6381d
Zona-firman <zonafirman2@gmail.com>
2025-12-07 10:29:08 +0700

    perbaikan 3

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
